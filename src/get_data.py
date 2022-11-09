c268e84e20602563ecc9287f06698aa17f319820
praveen <Praveen-Kumar-SGK>
2022-11-09 20:07:01 +0530

    First Commit

---
+++ src/get_data.py
@@ -0,0 +1,28 @@
+# Purpose of this file is to
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def read_params(config_path):
+    with open(config_path) as f:
+        config=yaml.safe_load(f)
+    return config
+
+def get_data(config_path):
+    config=read_params(config_path)
+    data_path=config["data_source"]["s3_source"]
+    data=pd.read_csv(data_path,sep=",",encoding="utf-8")
+    print(data.head())
+
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument('--config',default="params.yaml")
+    parsed_args=args.parse_args()
+    get_data(parsed_args.config)
+
+
+
+
