be916bf21a6ad2746e10566aea9611ddd8cda664
Praveen-Kumar-SGK <109057699+Praveen-Kumar-SGK@users.noreply.github.com>
2022-11-14 18:45:57 +0530

    Prediction Service and App.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,jsonify
+import os
+import numpy as np
+from prediction_service import prediction
+
+static_dir = os.path.join('webapp','static')
+template_dir = os.path.join('webapp','templates')
+
+app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 
 
-app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.methods=='POST':
+        try:
+            if request.form:
+                dict_req=dict(request.form)
+                response=prediction.form_response(dict_req)
+                return render_template('index.html',response=response)
+            elif request.json:
+                response=prediction.api_response(request.json)
+        except Exception as e:
+            print(e)
+            error = {"Error": e}
+            return render_template('404.html',error=error)
+    else:
+        render_template("index.html")
 
-@app.route('/')
-def
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ prediction_service/predictions.py
@@ -0,0 +1,75 @@
+import os
+import yaml
+import json
+import numpy as np
+import joblib
+
+params_path='params.yaml'
+schema_path = os.path.join('prediction_service','schema.json')
+class NotInRange(Exception):
+    def __init__(self,message='Values entered are not in expected range'):
+        self.message = message
+        super().__init__(self.message)
+
+class NotInCols(Exception):
+    def __init__(self,message='Not in Columns'):
+        self.message =message
+        super().__init__(self.message)
+
+def read_params(config_path=params_path):
+    with open(config_path) as f:
+        config=yaml.safe_load(f)
+    return config
+
+def predict(data):
+    config=read_params(params_path)
+    model_dir=config['webapp_model_dir']
+    model=joblib.load(model_dir)
+    prediction=model.predict(data).tolist()[0]
+    try:
+        if 3 <= prediction <=8:
+            return prediction
+        else:
+            raise NotInRange
+    except NotInRange:
+        return "Unexpected result"
+
+def validate_input(dict_request):
+    def validate_cols(col):
+        schema = json.load(schema_path)
+        actual_col= schema.keys()
+        if col not in actual_col:
+            raise NotInCols
+
+    def validate_values(col,val):
+        schema=json.load(schema_path)
+        if not(schema[col]['min']<=float(dict_request[col])<=schema[col]['max']) :
+            raise NotInRange
+
+    for col,val in dict_request.items():
+        validate_cols(col)
+        validate_values(col,val)
+    return True
+
+def form_response(dict_request):
+    if validate_input(dict_request):
+        data = dict_request.values()
+        data = [list(map(float,data))]
+        response = predict(data)
+        return response
+    else:
+        return "Please check the data"
+
+def api_response(dict_request):
+    try:
+        if validate_input(dict_request):
+            data= np.array([dict_request.values()])
+            response = predict(data)
+            response= {'response':response}
+            return response
+    except NotInRange as e:
+        response = {'Expected values in Range': json.load(schema_path),'response':str(e)}
+    except NotInCols as e:
+        response = {"Expected Col":json.load(schema_path),'response':str(e)}
+    except Exception as e:
+        response={'Error':str(e)}
