720ba72e108cd15499493d8b6c50b68b026e3434
Praveen-Kumar-SGK <109057699+Praveen-Kumar-SGK@users.noreply.github.com>
2022-11-16 11:53:25 +0530

    split_data.py updated

--- src/split_data.py
+++ src/split_data.py
@@ -1,23 +1,30 @@
-import pandas as pd
-
-from get_data import read_params
+# split the raw data 
+# save it in data/processed folder
+import os
 import argparse
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from get_data import read_params
 
-def split_data(config_path):
-    config=read_params(config_path)
-    test_size=config['split_data']['test_size']
-    train_path=config['split_data']['train_path']
-    test_path=config['split_data']['test_path']
-    random_state=config['base']['random_state']
-    path=config['load_data']['raw_dataset_csv']
-    df=pd.read_csv(path,sep=',')
-    train,test=train_test_split(df,test_size=test_size,random_state=random_state)
-    train.to_csv(train_path,sep=",", index=False, encoding="utf-8")
-    test.to_csv(test_path,sep=",",index=False,encoding='utf-8')
+def split_and_saved_data(config_path):
+    config = read_params(config_path)
+    test_data_path = config["split_data"]["test_path"] 
+    train_data_path = config["split_data"]["train_path"]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    split_ratio = config["split_data"]["test_size"]
+    random_state = config["base"]["random_state"]
 
-if __name__=='__main__':
-    args=argparse.ArgumentParser()
-    args.add_argument('--config',default='params.yaml')
-    parsed=args.parse_args()
-    split_data(parsed.config)
+    df = pd.read_csv(raw_data_path, sep=",")
+    train, test = train_test_split(
+        df, 
+        test_size=split_ratio, 
+        random_state=random_state
+        )
+    train.to_csv(train_data_path, sep=",", index=False, encoding="utf-8")
+    test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    split_and_saved_data(config_path=parsed_args.config)
