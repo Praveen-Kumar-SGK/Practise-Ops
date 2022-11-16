451759796983a4b2a8ebc9bb5690d492182693b6
Praveen-Kumar-SGK <109057699+Praveen-Kumar-SGK@users.noreply.github.com>
2022-11-16 11:58:08 +0530

    split_data.py updated

--- src/load_data.py
+++ src/load_data.py
@@ -6,6 +6,7 @@ import argparse
 def load_and_save(config_path):
     config=read_params(config_path)
     df=get_data(config_path)
+    df=df.iloc[:,1:]
     new_col= [i.replace(" ","_") for i in df.columns]
     raw_data_path=config["load_data"]["raw_dataset_csv"]
     df.to_csv(raw_data_path,sep=",",header=new_col)
