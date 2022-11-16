55a9d8abc4ea7a5947b44c9f37e1b4dad5ad3ba2
Praveen-Kumar-SGK <109057699+Praveen-Kumar-SGK@users.noreply.github.com>
2022-11-16 10:57:55 +0530

    Updated files with errors

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,57 +1,91 @@
+# load the train and test
+# train algo
+# save the metrices, params
 import os
-import json
-import joblib
-import argparse
-import numpy as np
+import warnings
+import sys
 import pandas as pd
-
-from get_data import read_params
+import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
-from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
-def metrics(actual,pred):
-    rmse=np.sqrt(mean_squared_error(actual,pred))
-    mae=mean_absolute_error(actual,pred)
-    r2=r2_score(actual,pred)
-    return rmse,mae,r2
+from get_data import read_params
+import argparse
+import joblib
+import json
 
-def train_evaluate(config_path):
-    config=read_params(config_path)
-    train=pd.read_csv(config['split_data']['train_path'],sep=',')
-    test=pd.read_csv(config['split_data']['test_path'],sep=',')
-    train_x=train.drop([config['base']['target_col']],axis=1)
-    train_y=train[config['base']['target_col']]
-    test_x=test.drop([config['base']['target_col']],axis=1)
-    test_y=test[[config['base']['target_col']]]
-    # model=config['estimators']['ElasticNet']
-    l1_ratio=config['estimators']['ElasticNet']['params']['l1_ratio']
-    alpha=config['estimators']['ElasticNet']['params']['alpha']
-    lr=ElasticNet(l1_ratio=l1_ratio , alpha=alpha)
-    lr.fit(train_x,train_y)
-    pred=lr.predict(test_x)
-    rmse, mae, r2=metrics(test_y,pred)
+
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+
+def train_and_evaluate(config_path):
+    config = read_params(config_path)
+    test_data_path = config["split_data"]["test_path"]
+    train_data_path = config["split_data"]["train_path"]
+    random_state = config["base"]["random_state"]
+    model_dir = config["model_dir"]
+
+    alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
+    l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
+
+    target = [config["base"]["target_col"]]
+
+    train = pd.read_csv(train_data_path, sep=",")
+    test = pd.read_csv(test_data_path, sep=",")
+
+    train_y = train[target]
+    test_y = test[target]
+
+    train_x = train.drop(target, axis=1)
+    test_x = test.drop(target, axis=1)
+
+    lr = ElasticNet(
+        alpha=alpha,
+        l1_ratio=l1_ratio,
+        random_state=random_state)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
     print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
     print("  RMSE: %s" % rmse)
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
-#----------------------------------------------------------------------------------------------
-    params_path=config['reports']['params']
-    scores_path=config['reports']['scores']
 
-    with open(params_path,'w') as p:
-        parameters={"l1_ratio":l1_ratio,"alpha":alpha}
-        json.dump(parameters,p,indent=2)
+    #####################################################
+    scores_file = config["reports"]["scores"]
+    params_file = config["reports"]["params"]
 
-    with open(scores_path,'w') as s:
-        scores = {"RMSE":rmse,"MAE":mae,"R2":r2}
-        json.dump(scores,s,indent=2)
-#----------------------------------------------------------------------------------------------
-    model_path=os.path.join(config['model_dir'],"ElasticNet.joblib")
-    joblib.dump(lr,model_path)
+    with open(scores_file, "w") as f:
+        scores = {
+            "rmse": rmse,
+            "mae": mae,
+            "r2": r2
+        }
+        json.dump(scores, f, indent=4)
+
+    with open(params_file, "w") as f:
+        params = {
+            "alpha": alpha,
+            "l1_ratio": l1_ratio,
+        }
+        json.dump(params, f, indent=4)
+    #####################################################
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "ElasticNet.joblib")
+
+    joblib.dump(lr, model_path)
 
 
-
-if __name__=='__main__':
-    args=argparse.ArgumentParser()
-    args.add_argument('--config',default='params.yaml')
-    parsed=args.parse_args()
-    train_evaluate(parsed.config)
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
